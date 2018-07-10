bbec1f60e255c1f8ec7eede35a8f225d29703375
Adham Muhammad <admuaq@gmail.com>
2018-07-10 17:39:32 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,6 +2,8 @@ def my_select(collection)
  # your code here!
  cont = []
  i = 0 
- while i < collection.length
-  
+  while i < collection.length
+    cont << yield(collection[i])
+    i+= 1 
+  end 
 end
